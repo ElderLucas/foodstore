3f819ac6f0122f361cd6047cd068060030ec141b
Elder Lucas <elderlucas@gmail.com>
2016-12-10 13:59:13 -0200

    Update gen_file_virtualhost.sh

--- gen_file_virtualhost.sh
+++ gen_file_virtualhost.sh
@@ -5,14 +5,15 @@
 # Entrada $2 = Nome do arquivo de configuração do VirtualHols, que tem extesão ".com.conf" e fica dentro do diretório /etc/apache2/sites-available
 # Entrada $3 = Diretório de saída do arquivo que vai ser gerado pelo script, geralmente deve ser /etc/apache2/sites-available
 ##################################################################################################################################
-domain_name=$1
-file_name=$2
-output_dir=$3
+domain_name=$1 #Entrada 01
+file_name=$2 #Entrada 02
+output_dir=$3 #Entrada 03
+dir_name=$4 #Entrada 04
 
 ##################################################################################################################################
 # Esse é o nome defaul que é dado para o arquivo wsgi
 ##################################################################################################################################
-std_wsgi_name="project.wsgi"  
+std_wsgi_name="project.wsgi"
 
 ##################################################################################################################################
 # Início da geração do arquivo de configuração do Host
@@ -21,18 +22,18 @@ cat > $output_dir/$file_name".com.conf" << EOF1
 <VirtualHost *:80>
 
 	ServerName $domain_name
+
+    	WSGIScriptAlias / $dir_name/$std_wsgi_name
+        WSGIDaemonProcess python_app user=superpython group=sudo
+        WSGIProcessGroup python_app
 	
-    	WSGIScriptAlias / $(pwd)/$std_wsgi_name
-        WSGIDaemonProcess foodstoreapp user=foodstore group=sudo
-        WSGIProcessGroup foodstoreapp
-	
-    	<Directory $(pwd)/>
+    	<Directory $dir_name/>
         	Order deny,allow
         	Allow from all
     	</Directory>
 
-	Alias /static $(pwd)/static/
-	<Directory $(pwd)/static/>
+	Alias /static $dir_name/static/
+	<Directory $dir_name/static/>
 		Order allow,deny
 		Allow from all
 	</Directory>
